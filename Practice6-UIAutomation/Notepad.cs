42551cc46e13215b91504c1b79a3df8e99a296d8
BlakkBerry <blakkberry2015@gmail.com>
2021-03-16 18:37:30 +0200

    Finished lab 6

---
+++ Practice6-UIAutomation/Notepad.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using FlaUI.Core.Input;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+
+namespace Practice6_UIAutomation
+{
+    public class Notepad : IDisposable
+    {
+        private Window _window;
+
+        public Notepad()
+        {
+            var application = Application.Launch(@"C:\Windows\System32\notepad.exe");
+            var automation = new UIA3Automation();
+            _window = application.GetMainWindow(automation);
+        }
+
+        public void ZoomIn()
+        {
+            var scaleUpBtn = GetZoomMenuItems().Single(menuItem => menuItem.Name == "Увеличить");
+
+            scaleUpBtn.WaitUntilClickable();
+            scaleUpBtn.Click();
+        }
+
+        public void ZoomOut()
+        {
+            var scaleUpBtn = GetZoomMenuItems().Single(menuItem => menuItem.Name == "Уменьшить");
+
+            scaleUpBtn.WaitUntilClickable();
+            scaleUpBtn.Click();
+        }
+
+        public int GetZoomPercent()
+        {
+            var zoomText = _window.FindAllByXPath("/StatusBar/Text[3]")[0].Name;
+            var zoomNumberString = Regex.Match(zoomText, @"[0-9]+").ToString();
+            return Convert.ToInt32(zoomNumberString);
+        }
+
+        public void WriteToDocument(string text)
+        {
+            var document = _window.FindAllChildren().Single(element =>
+                element.ControlType == ControlType.Document && element.ClassName == "Edit");
+
+            document.Click();
+            Keyboard.Type(text);
+        }
+
+        public string OpenDocument(string path)
+        {
+            var openBtn = GetFileMenuItems().Single(menuItem =>
+                menuItem.ControlType == ControlType.MenuItem && menuItem.Name == "Открыть...");
+            openBtn.WaitUntilClickable();
+            openBtn.Click();
+            
+            Retry.WhileTrue(() => _window.ModalWindows.Length == 0);
+            var openingModal = _window.ModalWindows[0].AsWindow();
+
+            var nameEdit = openingModal.FindAllDescendants().Single(element =>
+                element.ControlType == ControlType.Edit && element.Name == "Имя файла:");
+            
+            Retry.WhileTrue(() => !nameEdit.FrameworkAutomationElement.HasKeyboardFocus);
+            Keyboard.Type(path);
+            
+            var openingBtn = _window.FindAllByXPath("/Window/Button[1]")[0];
+            openingBtn.WaitUntilClickable();
+            openingBtn.Click();
+
+            var document = _window.FindAllChildren().Single(element =>
+                element.ControlType == ControlType.Document && element.ClassName == "Edit");
+            
+            return document.AsTextBox().Text;
+        }
+
+        public void SaveDocument(string path)
+        {
+            var saveBtn = GetFileMenuItems().Single(menuItem =>
+                menuItem.ControlType == ControlType.MenuItem && menuItem.Name == "Сохранить");
+            saveBtn.WaitUntilClickable();
+            saveBtn.Click();
+                
+            Retry.WhileTrue(() => _window.ModalWindows.Length == 0);
+            var savingModal = _window.ModalWindows[0].AsWindow();
+
+            var nameEdit = savingModal.FindAllDescendants().Single(element =>
+                element.ControlType == ControlType.Edit && element.Name == "Имя файла:");
+
+            Retry.WhileTrue(() => !nameEdit.FrameworkAutomationElement.HasKeyboardFocus);
+            Keyboard.Type(path);
+
+            var savingBtn = savingModal.FindAllDescendants().Single(element =>
+                element.ControlType == ControlType.Button && element.Name == "Сохранить");
+            savingBtn.WaitUntilClickable();
+            savingBtn.Click();
+        }
+
+        public string GetWindowTitle()
+        {
+            return _window.Title;
+        }
+
+        private MenuItems GetZoomMenuItems()
+        {
+            var menuItems = GetMenuItems();
+            
+            var viewTab = menuItems.Single(menuItem =>
+                menuItem.ControlType == ControlType.MenuItem && menuItem.Name == "Вид");
+            viewTab.WaitUntilClickable();
+            viewTab.Click();
+            
+            var viewMenu = _window.FindAllDescendants().Single(element =>
+                element.ControlType == ControlType.Menu && element.Name == "Вид");
+            
+            var scaleTab = viewMenu.FindAllChildren().Single(
+                menuItem => menuItem.ControlType == ControlType.MenuItem && menuItem.Name == "Масштаб");
+            scaleTab.WaitUntilClickable();
+            scaleTab.Click();
+            
+            var scaleMenu = _window.FindAllDescendants().Single(element =>
+                element.ControlType == ControlType.Menu && element.Name == "Масштаб");
+
+            return scaleMenu.AsMenu().Items;
+        }
+
+        private MenuItems GetFileMenuItems()
+        {
+            var fileTab = GetMenuItems().Single(menuItem =>
+                menuItem.ControlType == ControlType.MenuItem && menuItem.Name == "Файл");
+            fileTab.WaitUntilClickable();
+            fileTab.Click();
+
+            return _window.FindAllChildren().Single(element =>
+                element.ControlType == ControlType.Menu && element.Name == "Файл").AsMenu().Items;
+        }
+        
+        private MenuItems GetMenuItems()
+        {
+            return _window.FindAllByXPath("/MenuBar")[0].AsMenu().Items;
+        }
+
+        public void Dispose()
+        {
+            _window.Close();
+        }
+    }
+}
---
+++ Practice6-UIAutomation/NotepadTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using FlaUI.Core;
+using FlaUI.Core.Tools;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Practice6_UIAutomation
+{
+    public class NotepadTest
+    {
+        private Notepad _notepad; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _notepad = new Notepad();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _notepad.Dispose();
+        }
+
+        [Test]
+        public void ZoomTest()
+        {
+            _notepad.GetWindowTitle().Should().Be("Безымянный – Блокнот");
+        
+            _notepad.GetZoomPercent().Should().Be(100, "it is a default notepad zoom value.");
+            
+            _notepad.ZoomIn();
+            _notepad.GetZoomPercent().Should().Be(110, "scale has been zoomed in by 10%");
+            
+            _notepad.ZoomOut();
+            _notepad.ZoomOut();
+            _notepad.GetZoomPercent().Should().Be(90, "scale has been zoomed out by 20%");
+        }
+
+        [Test]
+        public void WriteFile()
+        {
+            const string text = "Hello World!";
+            _notepad.WriteToDocument(text);
+            
+            const string path = @"C:\Users\blakk\OneDrive\Desktop\test.txt";
+            _notepad.SaveDocument(path);
+            
+            Retry.WhileTrue(() => !File.Exists(path));
+            Assert.That(File.Exists(path), "File has not been saved.");
+
+            _notepad.OpenDocument(path).Should().Be(text, "this text has been written to the document.");
+            File.ReadAllText(path).Should().Be(text, "this text has been written to the document.");
+            
+            File.Delete(path);
+        }
+    }
+}
